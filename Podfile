79f5e92feeb37e9e65ef9f63a937f39e720f363e
Michael Zhang <zhangchi198@gmail.com>
2018-12-15 21:19:40 -0500

    Initial commit.
    Added Google Sign In SDK and GoogleSheet API SDK using Cocaopod.

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '12.0'
+
+target 'Sign-In' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Sign-In
+  pod 'GoogleAPIClientForREST/Sheets'
+  pod 'GoogleSignIn'
+end
---
+++ Sign-In/Sign-In-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  SignInBridgingHeader.h
+//  Sign-In
+//
+//  Created by Michael Zhang on 2018-12-15.
+//  Copyright © 2018 CGCToronto. All rights reserved.
+//
+
+#ifndef Sign-In-Bridging-Header_h
+#define Sign-In-Bridging-Header_h
+
+#import <GTMSessionFetcher/GTMSessionFetcher.h>
+#import <GTMSessionFetcher/GTMSessionFetcherService.h>
+
+#endif /* Sign-In-Bridging-Header_h */
